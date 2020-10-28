a854469df85485e92427da2f6fcdd3c6581cfc39
schspa <schspa@gmail.com>
2020-10-28 14:36:00 +0800

    add regs reload & regname search.
    Signed-off-by: schspa <schspa@gmail.com>

--- devutils/static/js/init.js
+++ devutils/static/js/init.js
@@ -1,6 +1,13 @@
 function update_reg_list() {
+    var regtype = $('#regtype').val();
+    var url;
+    if (regtype == "AARCH64") {
+        reg_xml_url = '/static/xml/SysReg_xml_v86A-2019-12/AArch64-regindex.xml';
+    } else {
+        reg_xml_url = '/static/xml/SysReg_xml_v86A-2019-12/AArch32-regindex.xml';
+    }
     $.ajax({
-        url: '/static/xml/SysReg_xml_v86A-2019-12/AArch64-regindex.xml',
+        url: reg_xml_url,
         //url: '/static/xml/test.xml',
         type: "GET",
         error: function (request, status, error) {
@@ -8,8 +15,10 @@ function update_reg_list() {
             console.log("XML reading failed: ", error);
         },
         success: function(response) {
+            var regnames = new Array(0);
             console.log("get xml data successfully");
             var list = $('#regs-table');
+            list.empty();
             $(response)
             // and search for all <field> elements
                 .find('register_link')
@@ -20,19 +29,38 @@ function update_reg_list() {
                     // get the values we want
                     var label = field.text();
                     var id = field.attr('id');
+                    regnames.push(id);
                     // and append some html in the <dl> element we stored previously
                     list
                         .append('<tr> <th scope="row">' + id + '</th> <td>' + label+': </td> </tr>')
                     ;
                 })
             ;
+            // Constructing the suggestion engine
+            var regnames = new Bloodhound({
+                datumTokenizer: Bloodhound.tokenizers.whitespace,
+                queryTokenizer: Bloodhound.tokenizers.whitespace,
+                local: regnames
+            });
+
+            // Initializing the typeahead
+            $('#regname').typeahead({
+                hint: true,
+                highlight: true, /* Enable substring highlighting */
+                minLength: 1 /* Specify minimum characters required for showing result */
+            },
+                                    {
+                                        name: 'regnames',
+                                        source: regnames
+                                    });
+
 
             console.log('done');
         }
     })
 }
 
-$(document).ready(function (){
+$(document).ready(function () {
     update_reg_list();
 })
 
