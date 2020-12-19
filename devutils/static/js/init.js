761ad5e49618e69de9f188fa82eab02815f2db0c
schspa <schspa@gmail.com>
2020-12-19 21:39:34 +0800

    support partial_fieldset.
    Signed-off-by: schspa <schspa@gmail.com>

--- devutils/static/js/init.js
+++ devutils/static/js/init.js
@@ -113,10 +113,167 @@ function get_val_string(val, width, base) {
 
     return ("0".repeat(base_width) + ((val|0)+4294967296).toString(base)).substr(-base_width);
 }
+/**
+ *
+ * registers > register > reg_fieldsets
+ * registers > register > reg_fieldsets > fields > field > partial_fieldset
+ *  all node should contains subnode:
+ *      fields
+ *      reg_fieldset
+ *
+ */
+function parse_fieldsets(reg, regval) {
+    $('> reg_fieldset', reg).children('fieldat').each(function (index, element) {
+        var id = $(element).attr('id');
+        var msb = parseInt($(element).attr('msb'))
+        var lsb = parseInt($(element).attr('lsb'))
+        var width = msb - lsb + 1;
+
+        var value = regval >> lsb;
+        value &= (1 << width) - 1;
+
+        var field = $('> fields > field#'+id, reg);
+        var has_partial_fieldset = $(field).attr('has_partial_fieldset') === 'True';
+
+        console.log("get value", id, value, msb, lsb, width, has_partial_fieldset);
+        rwtype=$(field).attr('rwtype');
+        var fieldname = undefined;
+        if (rwtype != undefined) {
+            fieldname = rwtype;
+        } else {
+            fieldname = $('field_name:first', field).text();
+        }
+        regfields.push({
+            'id': id,
+            'name': fieldname,
+            'value': value,
+            'msb': msb,
+            'lsb': lsb,
+            'width': width,
+            'field': field,
+            'is_partial' : has_partial_fieldset
+        });
+    });
+}
+
+function get_partial_fildsets(reg, regval, cond) {
+    var partial_fieldset = undefined;
+
+    reg.find('partial_fieldset').each(function(index, element) {
+        instance = $('fields > fields_instance', element).text();
+        if (instance === cond) {
+            partial_fieldset = element;
+        }
+    });
+    if (partial_fieldset == undefined) {
+        console.log("Can't find Matching fields_instance");
+        return;
+    }
+
+    parse_fieldsets(partial_fieldset, regval);
+    console.log(partial_fieldset);
+    return '';
+}
+
+function get_reg_filed(reg, regfile, regval) {
+    regfields = new Array();
+    fieldesc = '';
+    var field_value_links = new Array();
+    var partial_fieldsets = new Array();
+
+    parse_fieldsets($('reg_fieldsets', reg), regval);
+
+    regfields.forEach(function(regfield) {
+        var field = regfield['field'];
+        var fieldname = regfield['name'];
+        var value = regfield['value'];
+        var width = regfield['width'];
+        var binvalue = get_val_string(value, width, 2);
+        var hexvalue = get_val_string(value, width, 16);
+        var decvalue = get_val_string(value, width, 10);
+
+        if (regfield['is_partial']) {
+            partial_fieldsets.push({'name' : fieldname, 'value' : value});
+        }
+        $(field).find('field_value_instance').each(function(idx, element) {
+            links = $('field_value_links_to', $(element));
+
+            var field_value = $('field_value', $(element)).text();
+            if (field_value == undefined || field_value !== ('0b' + binvalue)) {
+                return;
+            }
+            if (links == undefined || $(links).attr('linked_field_condition') == undefined) {
+                return;
+            }
+
+            field_value_links.push(
+                {
+                    'linked_field_condition' : $(links).attr('linked_field_condition'),
+                    'linked_field_name' : $(links).attr('linked_field_name'),
+                    'value' : $('field_value', $(element)).text(),
+                });
+        });
+    });
+    console.log(partial_fieldsets);
+    console.log(field_value_links);
+    partial_fieldsets.forEach(function (ele, index) {
+        regfield = regfields.find(function (regfield) {
+            return regfield['name'] === ele['name'];
+        });
+        if (regfield == undefined) {
+            return;
+        }
+
+        cond = field_value_links.find(function (ele) {
+            return ele['linked_field_name'] == regfield['name'];
+        });
+        if (cond == undefined) {
+            return;
+        }
+
+        get_partial_fildsets(
+            $('reg_fieldsets > fields > field#'+regfield['id'], reg),
+            ele['value'],
+            cond['linked_field_condition']);
+    });
+
+    console.log('regfields: ', regfields);
+
+    regfields = regfields.filter(function(ele) {
+        return (ele['is_partial'] == undefined || !ele['is_partial'])
+    });
+    regfields.sort(function(a, b){return b['msb'] - a['msb']});
+    fieldbody = '';
+    regfields.forEach(function(reg) {
+        var value = reg['value'];
+        var width = reg['width'];
+        var binvalue = get_val_string(value, width, 2);
+        var hexvalue = get_val_string(value, width, 16);
+        var decvalue = get_val_string(value, width, 10);
+
+        if ((reg['msb'] + 1) % 32 == 0) {
+            fieldbody += '<tr class="firstrow">';
+        }
+
+        fieldbody += '<td class = "lr" colspan="'+ reg['width'] + '">' + '<a class="binval" target="_blank" href="' + regfile + '#' + reg['id'] + '">' + reg['name'] + '</a>' + '</td>';
+
+        if (reg['lsb'] % 32 == 0) {
+            fieldbody += '</tr>';
+        }
+
+        fieldesc += '<tr><th>'+ reg['msb'] +':' + reg['lsb'] + '</th><th>' + reg['name'] + '</th> <td><a class="binval" target="_blank" href="' + regfile + '#' + reg['id'] + '">' + binvalue + '</a><div class="hexvalue">' + hexvalue + '</div><div class="decvalue">' + decvalue + '</div></td></tr>';
+    });
+
+    return {
+        'fieldbody' : fieldbody,
+        'fieldesc' : fieldesc,
+    };
+}
 
 function goto_regs_desc() {
     var regtype = $('#regtype').val();
     var regname = $('#regname').val();
+    /* TODO: switch to long.js or something else, to provide 64bit support */
     var regval  = parseInt($('#regvalue').val());
     var regfile = register_map[regname];
     console.log(regfile)
@@ -136,48 +293,9 @@ function goto_regs_desc() {
             filedescs.empty();
             var fieldbody = $('#regfield_body');
             fieldbody.empty();
-            regfields = new Array();
-            $('register_page > registers > register > reg_fieldsets > reg_fieldset',response)
-                .find('fieldat') /* arm use this element to get reg filed maps */
-                .each(function(index, element){
-                    // as example we query & store the field
-                    var id = $(element).attr('id');
-                    var msb = parseInt($(element).attr('msb'))
-                    var lsb = parseInt($(element).attr('lsb'))
-                    var width = msb - lsb + 1;
-
-                    var field = $('#'+id + ':first', response);
-                    rwtype=$(field).attr('rwtype');
-                    if (rwtype != undefined) {
-                        field = rwtype;
-                    } else {
-                        field = $('field_name:first', field).text();
-                    }
-                    regfields.push({'id': id, 'msb': msb, 'lsb': lsb, 'width': width, 'name': field});
-
-                    var value = regval >> lsb;
-                    value &= (1 << width) - 1;
-                    console.log("get value", id, value, msb, lsb, width);
-                    var binvalue = get_val_string(value, width, 2);
-                    var hexvalue = get_val_string(value, width, 16);
-                    var decvalue = get_val_string(value, width, 10);
-
-                    filedescs.append('<tr><th>'+ msb +':' + lsb + '</th><th>' + field + '</th> <td><a class="binval" target="_blank" href="' + regfile + '#' + id + '">' + binvalue + '</a><div class="hexvalue">' + hexvalue + '</div><div class="decvalue">' + decvalue + '</div></td></tr>');
-                });
-            regfields.sort(function(a, b){return b['msb'] - a['msb']});
-            result = '';
-            regfields.forEach(function(reg) {
-                if ((reg['msb'] + 1) % 32 == 0) {
-                    result += '<tr class="firstrow">';
-                }
-
-                result += '<td class = "lr" colspan="'+ reg['width'] + '">' + '<a class="binval" target="_blank" href="' + regfile + '#' + reg['id'] + '">' + reg['name'] + '</a>' + '</td>';
-
-                if (reg['lsb'] % 32 == 0) {
-                    result += '</tr>';
-                }
-            });
-            fieldbody.append(result);
+            result = get_reg_filed($('register_page > registers > register:first',response), regfile, regval);
+            fieldbody.append(result['fieldbody']);
+            filedescs.append(result['fieldesc']);
         }});
 }
 
